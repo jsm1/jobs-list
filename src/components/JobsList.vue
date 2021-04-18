563ec2c4380b6c5feeba47bd394138ba42df201b
jsm1 <jeremy@jeremy>
2021-04-18 18:39:48 +1000

    Job details page and some tweaks

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,43 +1,64 @@
 <template>
-    <div class="jobs-list">
-        <div class="jobs-fields jobs-field-headers">
-            <div class="job-field job-field--position">Position</div>
-            <div class="job-field job-field--type">Type</div>
-            <div class="job-field job-field--location">Location</div>
-            <div class="job-field job-field--closing">Closing</div>
-        </div>
-        <div
-            v-for="job in filteredJobs"
-            :key="job.Id"
-            class="jobs-fields">
-            <div class="job-field job-field--position">
-                <a :href="job.ApplyUrl" target="_blank">{{ job.Title }}</a>
-                <p>{{ job.Summary }}</p>
+    <div>
+        <div v-if="!selectedJob" class="jobs-list">
+            <div class="jobs-fields jobs-field-headers">
+                <div class="job-field job-field--position">Position</div>
+                <div class="job-field job-field--type">Type</div>
+                <div class="job-field job-field--location">Location</div>
+                <div class="job-field job-field--closing">Closing</div>
             </div>
-            <div class="job-field job-field--type">{{ job.workTypesList }}</div>
-            <div class="job-field job-field--location">{{ job.locationsList }}</div>
-            <div class="job-field job-field--closing">{{ job.closingDate }}</div>
-        </div>
-        <div class="job-filters">
             <div
-                v-for="filter in filterOptions"
-                :key="filter.label">
-                <p>{{ filter.label }}:</p>
+                v-for="job in filteredJobs"
+                :key="job.Id"
+                class="jobs-fields">
+                <div class="job-field job-field--position">
+                    <a :href="'#jobs/' + job.Id">{{ job.Title }}</a>
+                    <p>{{ job.Summary }}</p>
+                </div>
+                <div class="job-field job-field--type">{{ job.workTypesList }}</div>
+                <div class="job-field job-field--location">{{ job.locationsList }}</div>
+                <div class="job-field job-field--closing">{{ job.closingDate }}</div>
+            </div>
+            <div class="job-filters">
                 <div
-                    v-for="(transformedOption, option) in filter.options"
-                    :key="option">
-                    <label>
-                        <input v-model="appliedFilters[filter.field]" :value="option" type="checkbox">
-                        {{ transformedOption }}
-                    </label>
+                    v-for="filter in filterOptions"
+                    :key="filter.label">
+                    <p>{{ filter.label }}:</p>
+                    <div
+                        v-for="(transformedOption, option) in filter.options"
+                        :key="option">
+                        <label>
+                            <input v-model="appliedFilters[filter.field]" :value="option" type="checkbox">
+                            {{ transformedOption }}
+                        </label>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div
+            v-else
+            class="job-detail-view">
+            <p>{{ selectedJob.Title }}</p>
+            <a :href="selectedJob.ApplyUrl" target="_blank">Apply now</a>
+            <p>Job no: {{ selectedJob.Id }}</p>
+            <p>Work type: {{ selectedJob.workTypesList }}</p>
+            <p>Location: {{ selectedJob.locationsList }}</p>
+            <p>Categories: {{ selectedJob.categoriesList }}</p>
+            <div v-html="selectedJob.Overview"></div>
+            <p><strong>Advertised:</strong> {{ selectedJob.openingDate }}</p>
+            <p><strong>Applications close:</strong> {{ selectedJob.closingDateWritten }} {{ selectedJob.ClosingDateTimeZoneAbbr }}</p>
+            <p><a href="#">Back to search</a></p>
         </div>
     </div>
 </template>
 
 <script>
+import { DateTime } from 'luxon';
 import axios from 'axios';
+const DateFormat = {
+    DMY: 'd/L/y',
+    Written: 'd MMMM y'
+};
 export default {
     props: {
         url: {
@@ -48,6 +69,8 @@ export default {
     data() {
         return {
             data: null,
+            selectedJob: null,
+            selectedJobId: null,
             appliedFilters: {
                 WorkTypeList: [],
                 LocationList: [],
@@ -61,12 +84,12 @@ export default {
                 {
                     label: 'Locations',
                     field: 'LocationList',
-                    transformer: (value) => value.replace(/^[^|]+\|/, ''),
+                    transformer: this.formatField,
                 },
                 {
                     label: 'Categories',
                     field: 'CategoryList',
-                    transformer: (value) => value.replace(/^[^|]+\|/, ''),
+                    transformer: this.formatField,
                 },
             ],
         };
@@ -76,8 +99,11 @@ export default {
             return (this.data || []).map((job) => {
                 const j = { ...job };
                 j.workTypesList = j.WorkTypeList.join(', ');
-                j.locationsList = j.LocationList.map((location) => location.replace(/^[^|]+\|/, '')).join(', ');
-                j.closingDate = this.getClosingDate(j);
+                j.locationsList = j.LocationList.map(this.formatField).join(', ');
+                j.categoriesList = j.CategoryList.map(this.formatField).join(', ');
+                j.closingDate = this.getClosingDate(j, DateFormat.DMY);
+                j.openingDate = this.getOpeningDate(j);
+                j.closingDateWritten = this.getClosingDate(j, DateFormat.Written);
                 return j;
             });
         },
@@ -119,26 +145,59 @@ export default {
                 .filter(([, value]) => value.length > 0);
         },
     },
+    watch: {
+        selectedJobId() {
+            if (this.selectedJobId) {
+                this.selectedJob = this.jobs.find((j) => j.Id === this.selectedJobId);
+            } else {
+                this.selectedJob = null;
+            }
+        },
+        jobs() {
+            this.onHashChange();
+        },
+    },
     created() {
+        window.addEventListener('hashchange', this.onHashChange);
         axios.get(this.url)
             .then((resp) => {
                 this.data = resp.data;
             });
     },
     methods: {
-        getClosingDate(job) {
+        getClosingDate(job, format) {
             if (!job.ClosingDateUtc) {
                 return '-';
             }
-            const dateString = job.ClosingDateUtc.replace(/^.+\(([0-9]+)\).+$/, '$1');
+            return this.getDateAsString(job.ClosingDateUtc, job.ClosingDateUtcOffset, format);
+        },
+        getDateAsString(string, offset, format) {
+            const dateString = string.replace(/^.+\(([0-9]+)\).+$/, '$1');
             if (!dateString) {
                 return '-';
             }
             const parsed = new Date(parseInt(dateString));
-            const month = parsed.getMonth() + 1;
-            const day = parsed.getDate();
-            const year = parsed.getFullYear();
-            return `${day}/${month}/${year}`;
+            const luxon = DateTime.fromJSDate(parsed);
+            return luxon.setZone('UTC+' + offset).toFormat(format);
+        },
+        getOpeningDate(job) {
+            if (!job.OpeningDateUtc) {
+                return '-';
+            }
+            return this.getDateAsString(job.OpeningDateUtc, job.OpeningDateUtcOffset, DateFormat.Written);
+        },
+        formatField(fieldValue) {
+            return (fieldValue || '').replace(/^[^|]+\|/, '');
+        },
+        onHashChange() {
+            const hash = window.location.hash;
+            const regex = /^.+jobs\/([a-zA-Z0-9]+)[^a-zA-Z0-9]*$/;
+            const matches = hash.match(regex);
+            if (matches) {
+                this.selectedJobId = matches[1];
+            } else {
+                this.selectedJobId = null;
+            }
         },
     },
 }
